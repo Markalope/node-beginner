2188a292c8905dfdffcb6c5168fa95ed7d8b6ebe
Mark Myers <markalope@gmail.com>
2018-07-03 13:27:57 -0700

    finished building the POST files

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>' +
@@ -16,17 +16,17 @@ function start(response) {
         '</body>' +
         '</html>';
     
-    
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
         response.end();
 }
 
     
-function upload(response) {
+function upload(response, postData) {
     console.log("request handler 'upload was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've Sent the text: " + 
+    querystring.parse(postData).text);
     response.end();
 }
 
