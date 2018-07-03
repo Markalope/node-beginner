00463cb9b7a9d86234a37cc698ecf50765802720
Mark Myers <markalope@gmail.com>
2018-07-02 17:02:57 -0700

    added timeout to requestHanders

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,19 @@
 function start() {
-    console.log("Request handler 'start' was called.")
+    console.log("Request handler 'start' was called.");
+    return "Hello Start";
 
+    function sleep(milliSeconds) {
+        var startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + milliSeconds);
+    }
+    sleep(10000);
+    return "Hello Start";
 }
+
 function upload() {
-    console.log("request handler 'upload was called.")
-
+    console.log("request handler 'upload was called.");
+    return "Hello Upload";
 }
+
 exports.start = start;
 exports.upload = upload;
--- server.js
+++ server.js
@@ -2,16 +2,13 @@ var http = require("http");
 var url = require("url");
 
 function  start(route, handle) {
-    console.log("Request handler 'start' was called.");
     function onRequest(request, response) {
-        const pathname = url.parse(request.url).pathname;
+        var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname);
-
-
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
+        var content = route(handle,pathname)
+        response.write(content);
         response.end();
 }
 
